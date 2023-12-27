a18ecacaf3e66d16d5901d58f70ea92c809e4e5b
Yulchikss <yuliapelia19@gmail.com>
2023-12-27 15:03:54 +0200

    feat: fix modal

--- src/modal-sellers.js
+++ src/modal-sellers.js
@@ -1,12 +1,13 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const openModalBtn = document.querySelector(".close_modal");
-    const closeModalBtn = document.querySelector(".open_modal");
-    const modal = document.querySelector(".modal__overlay");
+window.addEventListener('DOMContentLoaded', () => {
+  const openModalBtn = document.querySelector('.open_modal');
+  const closeModalBtn = document.querySelector('.close_modal');
+  const modal = document.querySelector('.modal__overlay');
 
-    openModalBtn.addEventListener('click', () => {
-        modal.classList.add("show");
-    });
-    closeModalBtn.addEventListener('click', () => {
-        modal.classList.remove("show");
-    });
+  openModalBtn.addEventListener('click', () => {
+    modal.classList.add('show');
+  });
+  closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+  });
+
 });
