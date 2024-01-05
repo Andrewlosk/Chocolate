5c90866e5c131cc53d16a3c0aff7387539aa1e27
Andrewlosk <114972263+Andrewlosk@users.noreply.github.com>
2024-01-05 18:03:00 +0200

    feat: fixing modal

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,4 +1,26 @@
-$(".slick__slider").slick({
-    dots:true,infinite:false,slidesToShow: 1,
-    slidesToScroll: 1,variableWidth: true
-})
+$('.slick__slider').slick({
+  dots: true,
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  adaptiveHeight: true,
+  responsive: [
+    {
+        breakpoint:1200, 
+        settings:{
+            slidesToShow: 3,
+
+        }
+
+    },
+    {
+        breakpoint: 768,
+        settings:{
+            slidesToShow:1,
+        }
+    }
+
+
+  ]
+});
