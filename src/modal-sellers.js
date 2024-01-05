04f79067327e76572b041790ed58cf205f37fb81
Andrewlosk <114972263+Andrewlosk@users.noreply.github.com>
2024-01-05 19:33:21 +0200

    feat: add function to modal

--- src/modal-sellers.js
+++ src/modal-sellers.js
@@ -1,13 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const openModalBtn = document.querySelector('.open_modal');
-  const closeModalBtn = document.querySelector('.close_modal');
-  const modal = document.querySelector('.modal__overlay');
+  const openModalBtn = document.querySelectorAll('[data-open-sellers]');
+  const closeModalBtn = document.querySelector('[data-close-sellers]');
+  const modal = document.querySelector('[data-backdrop-sellers]');
 
-  openModalBtn.addEventListener('click', () => {
-    modal.classList.add('show');
-  });
-  closeModalBtn.addEventListener('click', () => {
-    modal.classList.remove('show');
-  });
-
+  // openModalBtn.addEventListener('click', () => {
+  //   modal.classList.add('show');
+  // });
+  // closeModalBtn.addEventListener('click', () => {
+  //   modal.classList.remove('show');
+  // });
+  closeModalBtn.addEventListener("click", toggleModal1);
+  function toggleModal1() {
+    modal.classList.toggle('is-hidden-sellers');
+  }
+  openModalBtn.forEach(btn => btn.addEventListener('click', toggleModal1));
 });
