881671eefba6fb749852fd6ee97422fbd36b053b
Andrewlosk <114972263+Andrewlosk@users.noreply.github.com>
2024-01-10 19:51:19 +0200

    feat: finish section

--- src/modal-sellers.js
+++ src/modal-sellers.js
@@ -12,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
   closeModalBtn.addEventListener("click", toggleModal1);
   function toggleModal1() {
     modal.classList.toggle('is-hidden-sellers');
+    modal.classList.toggle("animate__zoomIn");
   }
   openModalBtn.forEach(btn => btn.addEventListener('click', toggleModal1));
 });
