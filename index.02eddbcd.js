35729f7680b6bbb8b08c606665937b07e79b27d9
YuliaPelia <yuliapelia19@gmail.com>
2024-01-12 12:38:53 +0000

    Deploying to gh-pages from @ Andrewlosk/Chocolate@0049c7e43ca18b6ffbf4398a43e7ea385b9c50ca 🚀

---
+++ index.02eddbcd.js
@@ -0,0 +1,2 @@
+$(".slick__slider").slick({dots:!0,infinite:!1,slidesToShow:1,slidesToScroll:1,variableWidth:!0,adaptiveHeight:!0,responsive:[{breakpoint:1200,settings:{slidesToShow:3}},{breakpoint:768,settings:{slidesToShow:1}}]});
+//# sourceMappingURL=index.02eddbcd.js.map
