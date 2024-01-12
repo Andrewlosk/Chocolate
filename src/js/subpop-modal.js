fd8b4cb9d7021690ace78b41753c7d4c2ad8dfbc
Aleksandra-15~ <sah02247@gmail.com>
2024-01-12 20:31:58 +0200

    feature

--- src/js/subpop-modal.js
+++ src/js/subpop-modal.js
@@ -1,14 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const openModalBtn = document.querySelector('.open__subpop');
-    const closeModalBtn = document.querySelector('.order__subpop__close');
-    const modal = document.querySelector('.order__subpop');
-  
-    openModalBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-      modal.classList.add('show');
-    });
-     closeModalBtn.addEventListener('click', () => {
-      modal.classList.remove('show');
-    }); 
-  });
-  
+window.addEventListener('DOMContentLoaded', () => { 
+  const openModalBtn = document.querySelectorAll('[data-open-sellers]'); 
+  const closeModalBtn = document.querySelector('[data-close-sellers]'); 
+  const modal = document.querySelector('[data-backdrop-sellers]'); 
+ 
+  closeModalBtn.addEventListener("click", toggleModal1); 
+  function toggleModal1() { 
+    modal.classList.toggle('is-hidden-sellers'); 
+    modal.classList.toggle("animate__zoomIn"); 
+    modal.classList.toggle('animate__zoomOut'); 
+  } 
+  openModalBtn.forEach(btn => btn.addEventListener('click', toggleModal1)); 
+});
