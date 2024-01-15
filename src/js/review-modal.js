0d0c733b24c3b729b26e8a69a7929d16033660fd
KaterinaZhuvak <katerinazhuvak@icloud.com>
2024-01-15 19:05:30 +0000

    feat:change in js 0.2

--- src/js/review-modal.js
+++ src/js/review-modal.js
@@ -1,13 +1,51 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const openModalBtn = document.querySelectorAll('[data-open-loved]');
-  const closeModalBtn = document.querySelector('[data-close-loved]');
-  const modal = document.querySelector('[data-backdrop-loved]');
+  // order-modal
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-open-loved]'),
+    closeModalBtn: document.querySelector('[data-close-loved]'),
+    modal: document.querySelector('[data-backdrop-loved]'),
+  };
 
-  closeModalBtn.addEventListener('click', toggleModal1);
-  function toggleModal1() {
-    modal.classList.toggle('is-hidden-loved');
-    modal.classList.toggle('animate__zoomIn');
-    modal.classList.toggle('animate__zoomOut');
+  refs.closeModalBtn.addEventListener('click', closeModal);
+
+  function openModal() {
+    refs.modal.classList.toggle('is-hidden-loved');
+    refs.modal.classList.add('animate__zoomIn');
+    refs.modal.classList.remove('animate__zoomOut');
   }
-  openModalBtn.forEach(btn => btn.addEventListener('click', toggleModal1));
+  function closeModal() {
+    refs.modal.classList.toggle('is-hidden-subscribe');
+    refs.modal.classList.remove('animate__zoomIn');
+    refs.modal.classList.add('animate__zoomOut');
+  }
+
+  refs.openModalBtn.forEach(btn => btn.addEventListener('click', openModal));
+
+  // thanks-modal
+
+  const thanksRefs = {
+    openModalBtn: document.querySelector('[data-open-thanks]'),
+    closeModalBtn: document.querySelector('[data-close-thanks]'),
+    modal: document.querySelector('[data-backdrop-thanks]'),
+  };
+
+  function openThanksModal() {
+    refs.modal.classList.add('is-hidden-subscribe');
+    thanksRefs.modal.classList.remove('is-hidden-thanks');
+    thanksRefs.modal.classList.toggle('animate__zoomIn');
+    refs.modal.classList.remove('animate__zoomIn');
+    refs.modal.classList.add('animate__zoomOut');
+    thanksRefs.modal.classList.remove('animate__zoomOut');
+  }
+
+  function closeThanksModal() {
+    thanksRefs.modal.classList.toggle('animate__zoomIn');
+    thanksRefs.modal.classList.add('animate__zoomOut');
+    thanksRefs.modal.classList.toggle('is-hidden-thanks');
+  }
+  document.addEventListener('click', function (event) {
+    event.preventDefault();
+    thanksRefs.openModalBtn.addEventListener('click', openThanksModal);
+    thanksRefs.closeModalBtn.addEventListener('click', closeThanksModal);
+  });
 });
